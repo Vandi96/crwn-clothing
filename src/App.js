f781526805cb4bd9a974558681495ad045327198
Vandi96 <49372844+Vandi96@users.noreply.github.com>
2019-07-23 12:15:25 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homapage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
+
 
 function App() {
   return (
     <div>
-      <Homapage />
+      <Switch>
+         <Route exact path='/' component = {Homapage} />
+         <Route path='/hats' component = {HatsPage} />
+      </Switch>
     </div>
   );
 }
